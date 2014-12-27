b6803ad11696467bf8707126657259c8cf1f4d26
tomgeekery <chris@commercialprogression.com>
2014-12-27 13:01:49 +0000

    Modified html tag to include language

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
   <head profile="<?php print $grddl_profile; ?>">
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
