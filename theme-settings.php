a779fbf4e615116874afe9b5114cc0dcbdd2e3f7
Chris Keller <chris@commercialprogression.com>
2015-01-15 14:30:16 -0500

    Replaced livereload with BrowserSync

--- theme-settings.php
+++ theme-settings.php
@@ -9,10 +9,11 @@ function stiff_drink_form_system_theme_settings_alter(&$form, $form_state) {
     '#title' => t('Stiff drink settings'),
   );
   
-  $form['stiff_drink_settings']['stiff_drink_livereload'] = array(
+  $form['stiff_drink_settings']['stiff_drink_browsersync'] = array(
     '#type' => 'checkbox',
-    '#title' => t('Enable livereload'),
-    '#default_value' => theme_get_setting('stiff_drink_livereload'),
+    '#title' => t('Enable BrowserSync'),
+    '#description' => t('Requires proper Grunt setup, use this:' . ' ' . l('https://bitbucket.org/alexfisher/compro_grunt', 'https://bitbucket.org/alexfisher/compro_grunt')),
+    '#default_value' => theme_get_setting('stiff_drink_browsersync'),
   );
   
 }
