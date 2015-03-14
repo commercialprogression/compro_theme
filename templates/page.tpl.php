ae27d6e3238fb12f1d3d67b9869160c311931803
Chris Keller <chris@commercialprogression.com>
2015-03-14 16:46:01 -0400

    Added some regions

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -5,16 +5,28 @@
     <?php print render($page['header']); ?>
   </header> <!-- /#header -->
 
-  <section id="content" class="column">
+  <section id="pre-content">
+    <?php print render($page['pre_content']); ?>
+  </section> <!-- /#pre-content -->
+
+  <section id="content">
     <a id="main-content"></a>
     <?php print render($page['content']); ?>
   </section> <!-- /#content -->
 
+  <section id="post-content">
+    <?php print render($page['post_content']); ?>
+  </section> <!-- /#post-content -->
+
   <?php if ($page['sidebar_first']): ?>
-    <aside id="sidebar-first" class="column sidebar">
+    <aside id="sidebar-first" class="sidebar">
       <?php print render($page['sidebar_first']); ?>
     </aside> <!-- /#sidebar-first -->
   <?php endif; ?>
+
+  <section id="pre-footer">
+    <?php print render($page['pre_footer']); ?>
+  </section> <!-- /#pre-footer -->
 
   <footer id="footer">
     <?php print render($page['footer']); ?>
