50be1ef1c72aa3117786e6470c4731a610988425
Chris Keller <chris@commercialprogression.com>
2017-06-02 11:25:43 +0000

    Added external links opening in new tabs JS

--- js/behavior.js
+++ js/behavior.js
@@ -4,6 +4,14 @@
 (function ($) {
   Drupal.behaviors.comproTheme = {
     attach: function (context, settings) {
+      // Give external links target="_blank"
+      var $a = $('a');
+      $a.each(function(i) {
+        if (this.href.length && this.hostname !== window.location.hostname) {
+          $(this).attr('target','_blank');
+        }
+      });
+    
       // Sliding panel
       $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
         $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
